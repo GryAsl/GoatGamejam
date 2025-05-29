ffa0a2e3f314c61f4153e13961645169a4380c31
GryAsl <86025991+GryAsl@users.noreply.github.com>
2025-05-29 20:38:21 +0300

    sonHali

---
+++ Assets/Scripts/AITest.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class AITest : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Vector3 target;
+    NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // Sol tık
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log("Tıklanan nokta: " + hit.point + " Tıklanan obje: " + hit.collider);
+                target = hit.point;
+                agent.destination = target;
+            }
+        }
+    }
+}
